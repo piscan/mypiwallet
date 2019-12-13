7e552444d882accc494446858a3f27634f8962a4
johnsavadkuhi <physitime@gmail.com>
2019-12-13 12:58:17 +0330

    add new routes and remove account route

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,9 @@ import '../style/main.scss'
 import Navigation from './navigation';
 import Account from './account/Account';
 import Wallet from './wallet'; 
-import Home from './home/Home'; 
+import Home from './home/NewHome'; 
+import CreateWallet from './account/CreateWallet';
+import ImportWallet from './account/ImportWallet'; 
 
 import {
   BrowserRouter as Router,
@@ -22,10 +24,10 @@ function App() {
 
         <Switch>
 
-          <Route exact path="/" component={Home}/>
-          <Route  path="/account" component={Account}/>
-          
+          <Route exact path="/" component={Home}/>          
           <Route path="/wallet" component={Wallet}/>
+          <Route path="/createwallet" component={CreateWallet}/>
+          <Route path="/importwallet" component={ImportWallet}/>
                  
         </Switch>
 
