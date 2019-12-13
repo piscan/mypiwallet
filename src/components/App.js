e96a22ca93578c6c3ed856487b3a26067175604d
johnsavadkuhi <physitime@gmail.com>
2019-12-14 02:15:08 +0330

    remove route settings to router component

--- src/components/App.js
+++ src/components/App.js
@@ -1,39 +1,13 @@
 import React from 'react';
 import '../style/main.scss'
-import Navigation from './navigation';
-import Wallet from './wallet'; 
-import Home from './home/NewHome'; 
-import CreateWallet from './account/CreateWallet';
-import ImportWallet from './account/ImportWallet'; 
-
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-
-} from "react-router-dom";
+import Router from './router';
 
 function App() {
 
   return (
-    
-      <Router >
 
-        <Navigation />
+      <Router/>
 
-        <Switch>
-
-          <Route exact path="/" component={Home}/>          
-          <Route path="/wallet" component={Wallet}/>
-          <Route path="/createwallet" component={CreateWallet}/>
-          <Route path="/importwallet" component={ImportWallet}/>
-                 
-        </Switch>
-
-      
-      </Router>
-
-  
   );
 }
 
