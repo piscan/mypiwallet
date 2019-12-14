9dae7ae5601adaea144269f2a834b15aec9265ea
johnsavadkuhi <physitime@gmail.com>
2019-12-14 23:24:39 +0330

    change p element to div

--- src/components/home/NewHome.js
+++ src/components/home/NewHome.js
@@ -1,31 +1,49 @@
 import React from 'react';
 // import { AnimateOnChange } from 'react-animation'
 import { animations } from 'react-animation'
+import { useQuery } from '@apollo/react-hooks';
+import { gql } from 'apollo-boost';
 
+const BALANCE = gql`
+    {
+        getBalance(address:"0xEA048c9D9B3D226550bDDb6515a6425153474D8b")
+    }
+`
 
 const style = {
     animation: animations.popIn
-}
+};
 
 function NewHome() {
 
+    const { loading, error, data } = useQuery(BALANCE);
+
+    console.log("loading : ", loading ); 
+    if(error){
+        console.log(error);
+        throw new Error(error);
+
+    }else {
+        console.log(data); 
+    }
+
     return (<>
 
-        <div id="bg" ></div>
+        <div id="bg" > </div>
 
         <main>
             <section  id="card" style={style}>
                 <ul >
                     <li>
 
-                        <p className="has-text-black  has-text-justified">
+                        <div className="has-text-black  has-text-justified">
                             <strong className="is-size-6-mobile is-size-5">Create A New Pchain Wallet  </strong>
                             <p>You can generate a pchain wallet easily with this web app. </p>
 
-                            </p>
+                            </div>
                     </li>
                     <li>
-                        <p className="has-text-black  has-text-left ">
+                        <div className="has-text-black  has-text-left ">
                             <strong className="is-size-6-mobile is-size-5"> Import A Wallet  </strong>
                             <p>
                                 You can import your wallet in two ways,
@@ -35,7 +53,7 @@ function NewHome() {
                             </p>
 
                              
-                        </p>
+                        </div>
 
                     </li>
                 </ul>
