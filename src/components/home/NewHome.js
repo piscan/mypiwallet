9087c5597c402f9938bb5361968124229f82c3be
johnsavadkuhi <physitime@gmail.com>
2019-12-14 21:00:31 +0330

    remove box class

--- src/components/home/NewHome.js
+++ src/components/home/NewHome.js
@@ -14,12 +14,12 @@ function NewHome() {
         <div id="bg" ></div>
 
         <main>
-            <section className="box" id="card" style={style}>
-                <ul>
+            <section  id="card" style={style}>
+                <ul >
                     <li>
 
                         <p className="has-text-black  has-text-justified">
-                            <strong>Create A New Pchain Wallet  </strong>
+                            <strong className="is-size-6-mobile is-size-5">Create A New Pchain Wallet  </strong>
                             <p>You can generate a pchain wallet easily with this web app. </p>
 
                             </p>
