519c817a9ab034bc1479f81284536ab383515936
johnsavadkuhi <physitime@gmail.com>
2019-12-16 16:14:06 +0330

    update

--- src/components/container/input/Password.js
+++ src/components/container/input/Password.js
@@ -7,76 +7,71 @@ import TextField from '@material-ui/core/TextField';
 import Visibility from '@material-ui/icons/Visibility';
 import VisibilityOff from '@material-ui/icons/VisibilityOff';
 import FormHelperText from '@material-ui/core/FormHelperText';
-import LockIcon  from '@material-ui/icons/LockOutlined';
-import Icon from '@material-ui/core/Icon';
+import styled from 'styled-components';
 
+const StyledTextField = styled(TextField)`
+ 
+label.Mui-focused {
+    color: black;
+  }
+  .MuiOutlinedInput-root {
+    fieldset {
+      border-color: white;
+    }
+    &:hover fieldset {
+      border : .5px solid #8CFFDD ; 
+    }
+    &.Mui-focused fieldset {
+      
+      border : .5px solid #74E8E6 ; 
+    }
+  }
+`;
 
-const useStyles = makeStyles(theme => ({
-    root: {
-      display: 'flex',
-      flexWrap: 'wrap',
-     
-    },
-    margin: {
-      margin: theme.spacing(1),
-    },
-    withoutLabel: {
-      marginTop: theme.spacing(3),
-    },
-    textField: {
-      width: 200,
-    },
-  }));
-
-  
 export default function Password(props) {
 
-    const classes = useStyles();
 
-    const [values, setValues] = React.useState({
-      showPassword: false,
-    });
-  
-    const handleClickShowPassword = () => {
-      setValues({ ...values, showPassword: !values.showPassword });
-    };
-  
-    const handleMouseDownPassword = event => {
-      event.preventDefault();
-    };
-  
-    return (
-      
-      <div className={classes.root}>
-    
-          <TextField
-          size="small"
-          label = {props.label}
-            type={values.showPassword ? 'text' : 'password'}
-            id="standard-start-adornment"
-            className="input"
-            variant="outlined"
-            placeholder=""
-            value={props.value}
-            onChange={props.onChange}
-            fullWidth
-            InputProps={{
-              endAdornment: <InputAdornment position="end">
-              <IconButton
-                aria-label="toggle password visibility"
-                onClick={handleClickShowPassword}
-                onMouseDown={handleMouseDownPassword}
-              >
-                {values.showPassword ? <Visibility  /> : <VisibilityOff />}
-              </IconButton>
-            </InputAdornment>
-             , startAdornment : <InputAdornment position="start">
-                 <Icon className ="fa fa-lock" style={{fontSize:'15px'}}/>
-             </InputAdornment>}}
-          />
-        <FormHelperText id="component-helper-text">Some important helper text</FormHelperText>
+  const [values, setValues] = React.useState({
+    showPassword: false,
+  });
 
-      </div>
-    );
-  }
-  
+  const handleClickShowPassword = () => {
+    setValues({ ...values, showPassword: !values.showPassword });
+  };
+
+  const handleMouseDownPassword = event => {
+    event.preventDefault();
+  };
+
+  return (
+
+    <div >
+
+      <StyledTextField
+        size="small"
+        label={props.label}
+        type={values.showPassword ? 'text' : 'password'}
+        id="standard-start-adornment"
+        className="input"
+        variant="outlined"
+        value={props.value}
+        onChange={props.onChange}
+        fullWidth        
+        InputProps={{
+          endAdornment: <InputAdornment position="end">
+            <IconButton
+              aria-label="toggle password visibility"
+              onClick={handleClickShowPassword}
+              onMouseDown={handleMouseDownPassword}
+            >
+              {values.showPassword ? <Visibility /> : <VisibilityOff />}
+            </IconButton>
+          </InputAdornment>
+        }}
+      />
+
+      <FormHelperText error={props.error} >{props.helper}</FormHelperText>
+
+    </div>
+  );
+}
