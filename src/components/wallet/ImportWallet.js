32eb9b1edfb707e43ef4e55dffa1477cb7f1bb63
johnsavadkuhi <physitime@gmail.com>
2019-12-16 20:20:51 +0330

    update

---
+++ src/components/wallet/ImportWallet.js
@@ -0,0 +1,21 @@
+import React from 'react'; 
+import FullWidthTabs from './../container/FullWidthTabs'; 
+
+function ImportWallet(){
+
+    return (<>
+    
+    <div >
+
+        
+
+    </div>
+    
+    
+    </>)
+
+}; 
+
+export default ImportWallet; 
+ 
+
