5596956b4e120c07a0f7b8907aa14f77ce4c7156
johnsavadkuhi <physitime@gmail.com>
2019-12-18 18:57:00 +0330

    update

--- src/components/wallet/ImportWallet.js
+++ src/components/wallet/ImportWallet.js
@@ -43,24 +43,18 @@ function ImportWallet(props) {
 
         setPrivateKey(e.target.value)
 
-        // if (e.target.value.length === 0) {
-        //     setHelper1('privateKey can not be empty.');
-        //     document.getElementById("helper1").classList.add('is-danger');
+        if (e.target.value.length === 0) {
+            setPkError({state:true , helper:'privateKey can not be empty.'})
 
+        } else if (e.target.value.length < 66) {
+            setPkError({state:true , helper:'the privateKey length is less than 64 chars.'})
 
-        // } else if (e.target.value.length < 66) {
-        //     setHelper1('the privateKey length is less than 64 chars.');
-        //     document.getElementById("helper1").classList.add('is-danger');
+          
+        }
+        else {
+            setPkError({state:false ,  helper:'Ok!'})
 
-
-        // }
-        // else {
-
-        //     document.getElementById("helper1").classList.remove('is-danger');
-        //     document.getElementById("helper1").classList.add('is-success');
-        //     setHelper1('Ok!');
-
-        // }
+        }
 
     }
     const handleChangePassword = e => {
@@ -84,21 +78,19 @@ function ImportWallet(props) {
 
         setName(e.target.value);
 
-        // if (localStorage.getItem(e.target.value) != null) {
-        //     setHelper2('Name already exists!');
-        //     document.getElementById('helper2').classList.add('is-danger'); 
-        // }
-        // else if (e.target.value.length === 0) {
-        //     setHelper2('This field can not be empty.');
-        //     document.getElementById('helper2').classList.add('is-danger'); 
+        if (localStorage.getItem(e.target.value) != null) {
+             setNameError({state : true , helper : 'Name already exists!'})
 
-        // } else {
-        //     setHelper2('Ok!');
-        //     document.getElementById('helper2').classList.remove('is-danger');
-        //     document.getElementById('helper2').classList.add('is-success'); 
- 
+        }
+        else if (e.target.value.length === 0) {
+            setNameError({state : true , helper : 'This field can not be empty.'})
 
-        // }
+          
+
+        } else {
+            setNameError({state : false , helper : 'Ok!'})
+
+        }
     }
 
     const handleImport = async () => {
@@ -144,7 +136,7 @@ if (redir) return <Redirect to="/" />
     
             <Name label="Name" onChange={handleChangeName} value={name} error={nameError.state} helper = {nameError.helper}/>
 
-            <Password value={password} onChange={handleChangePassword} status={passwordError.state} helper={passwordError.helper} 
+            <Password value={password} onChange={handleChangePassword} error={passwordError.state} helper={passwordError.helper} 
              label="Password" />
 
             <Textarea value={privateKey} onChange={handleChangePrivateKey} label ="PrivateKey" error={pkError.state}
